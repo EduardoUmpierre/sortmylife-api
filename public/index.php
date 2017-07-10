19e38622504178ae87cb91364e5d0667d49230bf
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2017-07-10 11:12:01 -0300

    CORS fix

--- public/index.php
+++ public/index.php
@@ -31,5 +31,18 @@ require __DIR__ . '/../services/Movie.php';
 // Register routes
 require __DIR__ . '/../src/routes.php';
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
+
 // Run app
 $app->run();
